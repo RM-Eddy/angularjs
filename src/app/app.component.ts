8efedf7499722f89927239c1624a81733986cfac
Eddy <tantelymicheleddy@gmail.com>
2024-07-26 14:42:08 +0300

    napiana ny title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { AjoutComponent } from './exemple/ajout/ajout.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'mon-projet-hello';
+  title = 'mon-projet-hello-test-git';
 }
